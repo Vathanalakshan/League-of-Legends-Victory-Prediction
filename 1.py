8f7c90cd89a9b838fd1870918782d86d6fa63a54
Vathanan <Vathanan@outlook.com>
2020-03-24 21:23:19 +0100

    Plotinng with panda

--- 1.py
+++ 1.py
@@ -1,11 +1,11 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import sklearn
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data=pd.read_csv('challenger.csv')
-data['win'].plot(kind='hist',bins=50,figsize=(12,6))
 Label = data['win']#La listes des wins
 data.drop(data.columns[0:19], axis=1, inplace=True)#La liste des donnees qu'on va utlisee
-print(Label)
+#ax = Label.plot.hist(bins=5) #Repartition des victoires
+data['damege_taken_20m_top_team_1'].plot.kde(c='red')
+data['damege_taken_20m_top_team_2'].plot.kde(c='blue')
+
+plt.show()
