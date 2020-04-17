ec8cb5170f68e9f923417ebed94bb1b16b176834
Vathanan <Vathanan@outlook.com>
2020-04-17 19:16:07 +0200

    1515

--- 1.py
+++ 1.py
@@ -5,37 +5,50 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns;
 
-data=pd.read_csv('challenger.csv')
+sns.set()
+
+data = pd.read_csv('challenger.csv')
 print(data.shape)
 
-data=data.drop_duplicates()
+data = data.drop_duplicates()
 
 print(data.shape)
-kill=["kills_bottom_duo_support_team_2","kills_bottom_duo_support_team_1","kills_bottom_duo_carry_team_2","kills_bottom_duo_carry_team_1","kills_top_team_1","kills_top_team_2","kills_middle_team_1","kills_middle_team_2","kills_jungle_team_1","kills_jungle_team_2"]
-k=[]
+kill = ["kills_bottom_duo_support_team_2", "kills_bottom_duo_support_team_1", "kills_bottom_duo_carry_team_2",
+        "kills_bottom_duo_carry_team_1", "kills_top_team_1", "kills_top_team_2", "kills_middle_team_1",
+        "kills_middle_team_2", "kills_jungle_team_1", "kills_jungle_team_2"]
+k = []
 for i, row in data.iterrows():
     for j in kill:
         if (row[j] > 40):
             k.append(i)
             break
 
-data=data.drop(k,axis=0)
+data = data.drop(k, axis=0)
 print(data.shape)
+kill1 = ["kills_top_team_1", "kills_jungle_team_1", "kills_middle_team_1", "kills_bottom_duo_carry_team_1",
+         "kills_bottom_duo_support_team_1"]
+kill2 = ["kills_top_team_2", "kills_jungle_team_2", "kills_middle_team_2", "kills_bottom_duo_carry_team_2",
+         "kills_bottom_duo_support_team_2"]
 
 
-sns.distplot(data["gold_earned_20m_top_team_2"])
-plt.show()
+def meanplotter(d, a):
+    mean = {}
+    for y in d:
+        if y in a:
+            mean[y] = (d[y].mean(axis=0))
+    print(mean)
 
-sns.pairplot(data,vars=kill)
-"""
-inputs = data.drop(data.columns[0:19],axis='columns')
-target = data["win"]
+    keys=mean.keys()
+    vals=mean.values()
 
-i_train,i_test,t_train,t_test=train_test_split(inputs[:100],target[:100],test_size=0.2)
-DTree= DecisionTreeClassifier()
-DTree.fit(i_train,t_train)
-print("100")
-print(DTree.score(i_test,t_test))
-"""
+    plt.bar(keys, np.divide(list(vals), sum(vals)), label="Real distribution")
+    plt.tight_layout()
+
+    plt.show()
+
+
+meanplotter(data,kill1)
+
+meanplotter(data,kill2)
